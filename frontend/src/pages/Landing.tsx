cc0deccde9993cc6ccd24bd4720cbcd01b857df0
varun789-mx <varunsh153@gmail.com>
2025-07-21 22:55:17 +0530

    remove .dev.vars

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,9 +1,23 @@
 import Navbarlogin from "../components/Navbar";
 
-const LandingPage = ()=>{ 
+import carImage from "../assets/Car_image.png"
+
+const LandingPage = () => {
     return (
         <div>
-<Navbarlogin/>
+            <Navbarlogin />
+            <div className="drop-shadow-blue-800 h-3/4 ">
+            <div className="absoulte inset-0 flex items-center">
+                <span>
+                    Your Journey
+                </span>
+                <br/>
+                <span>
+                    Your Car
+                </span>
+            </div>
+                <img src={carImage}/>
+            </div>
         </div>
     )
 }
