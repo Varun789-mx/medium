fe83b7642b4e11c551cb93b5713d03f199fb39ed
Harsh <VARUNSH153@GMAIL.COM>
2025-07-23 14:09:22 +0530

    Fixed the text on hero portion

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,25 +1,28 @@
 import Navbarlogin from "../components/Navbar";
 
-import carImage from "../assets/Car_image.png"
+import carImage from "../assets/Car_image.png";
 
 const LandingPage = () => {
-    return (
-        <div>
-            <Navbarlogin />
-            <div className="drop-shadow-blue-800 h-3/4 ">
-            <div className="absoulte inset-0 flex items-center">
-                <span>
-                    Your Journey
-                </span>
-                <br/>
-                <span>
-                    Your Car
-                </span>
-            </div>
-                <img src={carImage}/>
-            </div>
+  return (
+    <div>
+      <Navbarlogin />
+      <div className="drop-shadow-blue-800 h-3/4 ">
+        <div className="absolute inset-1.5 flex items-start text-7xl font-bold text-black ">
+          <div className="p-6  mt-60 align-top ">
+            <span>Your Journey</span>
+            <br />
+            <span>Your Car</span>
+          </div>
         </div>
-    )
-}
+        <div className="block h-2/3 overflow-hidden">
+        <img src={carImage} className="h-2/3" />
+      </div>
+      <div className="block h-2/3 overflow-hidden">
+        <img src={carImage} className="h-2/3" />
+      </div>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
