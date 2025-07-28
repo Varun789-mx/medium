053306d185b28fd97904e1be0af48b0a56a80c60
varun789-mx <varunsh153@gmail.com>
2025-07-28 22:58:16 +0530

    FIxed the login issue in login.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Navbarlogin from './components/Navbar'
 
 import LandingPage from './pages/Landing'
 import Login from './pages/Login'
@@ -8,7 +9,8 @@ import Signup from './pages/Signup'
 function App() {
   return (
     <>
-     <Login/>
+    <Navbarlogin/>
+      <Login />
     </>
   )
 }
